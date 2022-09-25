84394921dc687f56e3f69f1a3df4c002ce3a3b1c
Marcos8060 <marcosgav80@gmail.com>
2022-09-25 12:52:19 +0300

    added dashboard component

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
 
 const Dashboard = () => {
   return (
     <>
-    <div className="flex">
-        <div className='w-4/12 mx-auto'>
-            <h3>Marcos</h3>
+      <section className="bg-gray px-4 py-2">
+        <div className="flex bg-gray">
+          <div className="w-2/12 mx-auto text-center bg-white rounded py-2">
+            <FaUserCircle className="mx-auto text-2xl" />
+            <p>Marcos Ochieng</p>
+          </div>
+          <div className="w-10/12 mx-auto bg-gray py-4 px-4">
+            <button className="uppercase bg-blue text-white p-2 text-xs rounded font-semibold">Book appointment</button>
+          </div>
         </div>
-        <div className='w-8/12 mx-auto'>
-            Book an Appointment
-        </div>
-    </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,29 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      gray: "#E6E8EC",
+      white: "#FFFFFF",
+      blue: "#38A4FF",
+    },
+    height: {
+			"10v": "10vh",
+      "18v": "18vh",
+			"20v": "20vh",
+			"30v": "30vh",
+			"40v": "40vh",
+			"50v": "50vh",
+			"60v": "60vh",
+			"70v": "70vh",
+			"80v": "80vh",
+			"90v": "90vh",
+			"100v": "100vh",
+		},
   },
   plugins: [],
+  screens:{
+    'sm': '640px',
+    'md' : '768px',
+    'lg' : '1024px',
+  },
 }
