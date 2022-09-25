abb66e64b19e276d0c6bb838a3b2c387c90a1fea
Marcos8060 <marcosgav80@gmail.com>
2022-09-25 12:23:33 +0300

    initial commit

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+    <div className="flex">
+        <div className='w-4/12 mx-auto'>
+            <h3>Marcos</h3>
+        </div>
+        <div className='w-8/12 mx-auto'>
+            Book an Appointment
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
